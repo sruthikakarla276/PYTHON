48ba0035242ac817147ae74b53d764072316e499
Sruthikakarla <sruthikakarla276@gmail.com>
2025-09-19 18:56:29 +0530

    GRIDSYMBOLS1.py

---
+++ GRIDSYMBOLS1.py
@@ -0,0 +1,18 @@
+b=int(input("enter any number: "))
+a=[["A" for i in range(a+2)] [for i in range (a+2)]]
+for i in range(b+2):
+    for j in range(b+2):
+        if (i==0 or i ==b+1 or j==0 
+        elif(i%2!=0):
+           a[i][j]="*"
+       elif(i+j==8 and i<5):
+           a[i][j]="$"
+        
+for i in range(b+2):
+    for j in range(b+2):
+        print(a[i][j],end=" ")
+    
+    for j in range(star-1):
+            print("",end=" ")
+            count=count+1
+            print()
