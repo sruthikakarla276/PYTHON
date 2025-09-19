b336a6dfabf29efdaaace493cbf3241d885a39be
Sruthikakarla <sruthikakarla276@gmail.com>
2025-09-19 18:52:49 +0530

    CLOCK.py

--- CLOCK.py
+++ CLOCK.py
@@ -1,37 +1,4 @@
-'''import time
-print("hi")
-time.sleep(5)
-print("Meera")
 
-import time
-while True:
-    current_time = time.strftime("%H:%M:%S\n")
-    print(current_time, end="\r")
-    time.sleep(1)
-
-
-import time
-import sys
-h=4
-m=5
-s=0
-while(True):
-    sys.stdout.write(f"\r{h:02}:{m:02d}:{s:02d}")
-    sys.stdout.flush()
-    time.sleep(1)
-
-    s=s+1
-    if(s++60):
-        s=0
-        m=m+1
-    if(m++60):
-        m=0
-        s=0
-        h=h+1
-    if(h==12):
-        m=0
-        s=0
-        h=0'''
     
 a=20
 for i in range "1234" :
@@ -59,3 +26,4 @@ print(a)
 
 
     
+
